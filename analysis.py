0e61dc6c83fb610b3ffb1ed75c3f1a62fc2387f9
Loreenateng <loreenatenge121@gmail.com>
2025-10-15 17:29:14 +0300

    new upload

--- analysis.py
+++ analysis.py
@@ -1,20 +1,17 @@
 import pandas as pd
 
-# Load your dataset
+
 df = pd.read_csv("dirty_cafe_sales.csv")
-# Preview the first few rows
 print("First 5 rows:")
 print(df.head())
 
-# Check for missing values
 print("\nMissing values per column:")
 print(df.isnull().sum())
 
-# Summary info
 print("\nData info:")
 print(df.info())
 
-# Check top 10 most purchased items
+# Checking the top 10 most purchased items
 top_items = df['Item'].value_counts().head(10)
 print("\nTop 10 Most Purchased Items:")
 print(top_items)
